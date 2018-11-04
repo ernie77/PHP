373cc44ded12eb00e976bc5c898768b73b35c2c3
ernie77 <henri.maki@gmail.com>
2018-11-04 14:23:20 +0200

    database files

---
+++ php/addUser.php
@@ -0,0 +1,24 @@
+ <?php
+$servername = "localhost";
+$username = "library";
+$password = "";
+$dbname = "libraryDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO User (firstname, lastname)
+VALUES ('John', 'Doe')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
---
+++ php/createTables.php
@@ -0,0 +1,45 @@
+ <?php
+$servername = "localhost";
+$username = "library";
+$password = "";
+$dbname = "libraryDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create tables
+$sql = "CREATE TABLE Books (
+id INT(6) UNSIGNED AUTO_INCREMENT,
+isbn INT(13) NOT NULL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+author VARCHAR(255) NOT NULL,
+)";
+$sql = "CREATE TABLE User (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+)";
+$sql = "CREATE TABLE Laina (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Books_isbn INT(13) NOT NULL,
+User_id INT(6) NOT NULL,
+)";
+
+/*$sql = "CREATE TABLE Books (
+id INT(6) UNSIGNED AUTO_INCREMENT,
+isbn INT(13) NOT NULL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+author VARCHAR(255) NOT NULL,
+)";*/
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?> 
