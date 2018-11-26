1527ee24be0dc466cb07127a125af15ca097f09c
ernie77 <henri.maki@gmail.com>
2018-11-26 15:20:09 +0200

    tables

--- php/createTables.php
+++ php/createTables.php
@@ -12,28 +12,29 @@ if ($conn->connect_error) {
 }
 
 // sql to create tables
-$sql = "CREATE TABLE Books (
+$sql[0] = "CREATE TABLE Books (
 id INT(6) UNSIGNED AUTO_INCREMENT,
 isbn INT(13) NOT NULL PRIMARY KEY,
 title VARCHAR(255) NOT NULL,
 author VARCHAR(255) NOT NULL
 )";
-$sql = "CREATE TABLE User (
+$sql[1] = "CREATE TABLE User (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(255) NOT NULL,
 lastname VARCHAR(255) NOT NULL
 )";
-$sql = "CREATE TABLE Laina (
+$sql[2] = "CREATE TABLE Laina (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Books_isbn INT(13) NOT NULL,
 User_id INT(6) NOT NULL
 )";
-
-if ($conn->query($sql) === TRUE) {
+foreach ($sql as $aql) {
+//for ($x = 0; $x <= 10; $x++) {
+if ($conn->query($sql]) === TRUE) {
     echo "Table MyGuests created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
-
+}
 $conn->close();
 ?> 
