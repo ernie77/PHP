f304e179cf5f1a43b9b35db40f4f7435a1a43355
ernie77 <henri.maki@gmail.com>
2018-12-07 14:58:24 +0200

    addUser

--- php/addUser.php
+++ php/addUser.php
@@ -1,4 +1,5 @@
-<?php require 'test_input.php'
+<?php
+require('test_input.php');
 $servername = "127.0.0.1:49227";
 $username = "azure";
 $password = "6#vWHD_$";
@@ -28,12 +29,4 @@ $conn->close();
 sleep(5);
 header('location: ../index.html');
 exit;
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
 ?> 
