6f73e17ed83c479355339e1f0ec8375a49ff0281
ernie77 <henri.maki@gmail.com>
2018-12-09 11:23:11 +0200

    .

---
+++ php/removeBook.php
@@ -0,0 +1,25 @@
+ <?php
+$servername = "127.0.0.1:49227";
+$username = "azure";
+$password = "6#vWHD_$";
+$dbname = "libraryDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// values will come from webform
+$isbn = $_POST['isbn'];
+$sql = "DELETE FROM Books Where isbn = $isbn";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Created successfully";
+} else {
+    echo "Error creating: " . $conn->error;
+}
+
+$conn->close();
+?> 
